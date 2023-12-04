599f595c1079ebb4d41c4985576e660115ee0ee2
parkjiho <wlghsms987@gmail.com>
2023-12-04 17:58:36 +0900

    12/04 jiho

---
+++ jwt_jpa/src/test/java/com/domaji/jwt_jpa/JwtJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domaji.jwt_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
